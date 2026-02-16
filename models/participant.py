a0c6d1f3bcc06626ac2b35373ccee1d336dc81ec
khoivnk25411e <khoivnk25411e@st.uel.edu.vn>
2026-02-16 09:40:49 +0700

    create models

---
+++ models/participant.py
@@ -0,0 +1,6 @@
+class Participant:
+    def __init__(self, participant_id=None, participant_Name=None, email=None, phone_number=None):
+        self.participant_id = participant_id
+        self.participant_Name = participant_Name
+        self.email = email
+        self.phone_number = phone_number
