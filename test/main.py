bc49372e3fa7fd7cf08396c844ae942950b124c1
dangkhoa <khoantdk25411e@st.uel.edu.vn>
2026-02-21 21:12:55 +0700

    fix

--- test/main.py
+++ test/main.py
@@ -1,10 +1,13 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from Ui_Ex.LoginWindowEx import LoginWindowEx
+
+from Ui_ex.LoginWindowEx import LoginWindowEx
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
     main_window = LoginWindowEx()
     main_window.setupUi(QMainWindow())
     main_window.showWindow()
+
     sys.exit(app.exec())
